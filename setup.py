8b50178ed3e1f07054f7b2abf133f0ad2fb2cb72
jcristol <joshcristol@gmail.com>
2018-04-04 23:50:45 -0500

    changed some stuff in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 # Add here console scripts and other entry points in ini-style format
 entry_points = """
 [console_scripts]
-script_name = motifi.script:run
+motifi = motifi.script:run
 """
 
 
