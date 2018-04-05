3bde3ce63fb200d56721d8c24032f273bd9eed31
jcristol <joshcristol@gmail.com>
2018-04-04 21:52:16 -0500

    added an api wrapper in moitifi created the papertrade function

---
+++ src/motifi/api.py
@@ -0,0 +1,25 @@
+from threading import Thread
+from queue import Queue
+from rhwrapper.Robinhood import Robinhood
+
+class RobinhoodFetcher():
+
+  def __init__(self, trader: Robinhood):
+    self.rb = trader
+
+  def getInstruments(self, targets: list) -> list:
+    todo, results = Queue(len(targets)), []
+
+    def getInstrument() -> dict:
+      item = todo.get()
+      result = self.rb.quote_data(item['symbol'])
+      results.append(result)
+      todo.task_done()
+
+    for item in targets:
+      t = Thread(target=getInstrument)
+      t.daemon = True
+      t.start()
+      todo.put(item)
+    todo.join()
+    return results
--- src/motifi/motifi.py
+++ src/motifi/motifi.py
@@ -1,4 +1,17 @@
+import motifi.spreadsheet as sheets
+from rhwrapper.Robinhood import Robinhood
 
 def run(cash: float, spreadsheet: str):
+  targets: list = sheets.read_excel(spreadsheet)
+  m = Motifi()
 
-  pass
+
+
+class Motifi():
+
+  def __init__(self: Motifi):
+    self.rb = Robinhood()
+
+  def paperTrade(self: Motifi, cash: float, targets: list) -> list:
+    # get information from the api 
+    # return the information
