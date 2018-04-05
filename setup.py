f4708ec121cc5da57b656c874744fa33a347598d
jcristol <joshcristol@gmail.com>
2018-04-05 17:42:39 -0500

    fixed the problem by adding a path variable in zsh probably a temporary fix

--- setup.py
+++ setup.py
@@ -202,7 +202,7 @@ setup(
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
     entry_points = {
-        'console_scripts': ['funniest-joke=motifi.script:run']
+        'console_scripts': ['motifi=motifi.script:run']
     }
 
     # List additional URLs that are relevant to your project as a dict.
