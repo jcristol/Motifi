67b3d48173c8aa6e9cdf2fa54d0f971740ecc8b1
jcristol <joshcristol@gmail.com>
2018-04-05 16:09:27 -0500

    Setuptools will generate a standalone script ‘shim’ which imports your module and calls the registered function not being genereated.

--- setup.py
+++ setup.py
@@ -201,11 +201,9 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        'console_scripts': [
-            'motifi=motifi.script:run',
-        ],
-    },
+    entry_points = {
+        'console_scripts': ['funniest-joke=motifi.script:run']
+    }
 
     # List additional URLs that are relevant to your project as a dict.
     #
