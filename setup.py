a27e3991596a5992f2116877f0a5f3b1703f5ef0
jcristol <joshcristol@gmail.com>
2018-04-04 23:46:05 -0500

    changed setup.py to add a script call

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ from setuptools import setup
 # Add here console scripts and other entry points in ini-style format
 entry_points = """
 [console_scripts]
-# script_name = motifi.module:function
-# For example:
-# fibonacci = motifi.skeleton:run
+script_name = motifi.script:run
 """
 
 
